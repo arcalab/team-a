4273c11f2d3e222f5f7cf87e60a1c484d9d4a494
José Proença <jose@proenca.org>
2021-03-11 15:31:27 +0000

    toStrings...

--- src/main/scala/fta/eta/ST.scala
+++ src/main/scala/fta/eta/ST.scala
@@ -6,10 +6,19 @@ case class ST(snd:SRange,rcv:SRange):
   def valid():Boolean =
     !(snd.min == 0 && rcv.min == 0)
 
+  override def toString(): String =
+    s"$snd->$rcv"
+
+
 object ST: 
   case class SRange(min:Int,max:Option[Int]):
     def satisfies(n:Int):Boolean =
       min <= n && max.map(_>=n) != Some(false)
+    override def toString(): String = (min,max) match 
+      case (n,Some(m)) if n==m => n.toString
+      case (n,None) => s"$n..*"
+      case (n,Some(m)) => s"$n..$m"
+    
     
   object SRange:
     def apply(min:Int,max:Int) = new SRange(min,Some(max))
